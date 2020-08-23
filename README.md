3b007eee01aca1ec3a68b82b3d6ae0c753f36c0d
Horacio Herrera <hi@horacioh.com>
2020-08-23 04:32:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Create a custom hook
+
+## Steps
+
+- mostrar que estamos importando tailwind
+- mostrar la api (fake)
+- mostrar la app que carga el componente settings
+- comentar el componente de settings
+- crear el useState
+- create el useEffect
+- recap
---
+++ src/Settings.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Field from "./Field"
+
+function Settings() {
+  const [user, setUser] = React.useState({
+    id: "1234asdf",
+    email: "email@egghead.io",
+    firstName: "John",
+    lastName: "Doe",
+  })
+
+  return (
+    <div className="py-12">
+      <div className="mx-auto px-8 w-full max-w-3xl">
+        <h1 className="text-gray-800 font-bold text-lg py-2 border-b">
+          Settings
+        </h1>
+        {user ? (
+          <>
+            <Field label="First Name" value={user.firstName} />
+            <Field label="Last Name" value={user.lastName} />
+            <Field label="Email" value={user.email} />
+            <Field label="ID" value={user.id} />
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Settings
